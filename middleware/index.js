8621eff6ed9c48be685a502479f5817f8977c1bb
Parth <parthzarekar@gmail.com>
2023-12-24 14:26:30 +0530

    middleware

--- middleware/index.js
+++ middleware/index.js
@@ -5,42 +5,29 @@ const axios = require('axios');
 
 const app = express();
 const port = 3001; // Choose a port for your Node.js server
-app.use(cors())
+const corsOptions = { origin: '*', optionSuccessStatus: 200 };
+app.use(cors(corsOptions))
 app.use(express.json());
 
-app.use("*",(res,req) => res.status(404).json({error:"not found"}))
+// app.use("*",(res,req) => res.status(404).json({error:"not found"}))
 // Your custom authentication token
 const customToken = '12345678w';
 
 // Middleware for token authentication
-app.all(("/auth"), (req, res, next) => {
-  const userToken = req.header('Authorization');
-  console.log("UserToken: ", userToken)
-  // Check if the provided token matches the expected custom token
-  if (userToken == customToken || userToken == `Bearer ${customToken}`) {
-    // Token is valid, forward the request to the SpaceX API
-
-    // Extract the URL from the request
-    const url = req.query.url;
-
-    // Build the full URL for the SpaceX API
-    const spaceXApiUrl = `https://api.spacexdata.com/v3/capsules`;
-
-    // Forward the request to the SpaceX API
-    axios.get(spaceXApiUrl)
-      .then(response => {
-        // Forward the SpaceX API response to the React app
-        res.send(response.data);
-      })
-      .catch(error => {
-        // Handle errors from the SpaceX API
-        console.log("Hello")
-        res.status(error.response ? error.response.status : 500).send(error.message);
-
-      });
-  } else {
-    // Token is invalid, return an authentication error
-    res.status(401).json({ error: 'Authentication failed' });
+app.all(("/auth"), async (req, res, next) => {
+  try {
+    const userToken = req.header('Authorization');
+    console.log("UserToken: ", userToken)
+    if (userToken == customToken || userToken == `Bearer ${customToken}`) {
+      const spaceXApiUrl = `https://api.spacexdata.com/v3/capsules`;
+      const response = await axios.get(spaceXApiUrl);
+      res.send(response.data);
+    } else {
+      res.status(401).json({ error: 'Authentication failed' });
+    }
+  } catch (error) {
+    console.log("An error occurred: ", error);
+    res.status(error.response ? error.response.status : 500).send(error.message);
   }
 });
 
